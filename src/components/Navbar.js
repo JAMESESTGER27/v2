7cce0afcd274cd8866c14ccc86ca0004917489e8
JAMESESTGER27 <jamescapchaflores97@gmail.com>
2021-10-10 20:47:16 -0500

    corrigiendo cosas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useAuth } from '../auth/useAuth'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+import styles from "../styles/crud.module.css";
+
 
 export const Navbar = () => {
-    const auth = useAuth();
+    
+const [error, setError] = useState('')
+const { currentUser, logout } = useAuth()
+const history = useHistory()
 
+const handleClick = async () => {
+    setError('')
+
+    try {
+        await logout()
+        history.push('/')
+    } catch {
+        setError('Hubo un fallo al salir')
+    }
+}
     return (
-        <div>
-            <nav>
-                <ul>
-                    <li><Link to="/login">login</Link></li>
-                    <li><Link to="/register">register</Link></li>
-                    <li><Link to="/dashboard">dashboard</Link></li>
-                    <li><Link to="/payments">payments</Link></li>
-                    <li>
-                        <button
-                        onClick={auth.logout}
-                        >
-                            Salir
-                        </button>
-                    </li>
-
-                </ul>
-            </nav>
-        </div>
+        <>
+        <nav className={styles.nav}>
+            <div className={styles.container}>
+                <a href="/" className={styles.logo}>
+                    Aplication CRUD
+                </a>
+                <div className={styles.bar}>
+                    <h2>Bienvenido {currentUser.email}</h2>
+                    <button><Link to='/updateprofile'>Actualizar perfil</Link></button>
+                    <button onClick={handleClick}>Salir</button>
+                </div>
+            </div>
+        </nav>
+    { error && <h1>{error}</h1> }
+    </>
     )
 }
- 
--- src/pages/NotFounds404.js
+++ src/pages/NotFounds404.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import styles from "../styles/error.module.css"
 export const NotFounds404 = () => {
-    return (
-        <div>
-            NotFounds404
-        </div>
-    )
+	return (
+		<div>
+			<h1 style={{textAlign:"center"}}>404 Error Page</h1>
+			<p className={styles.zoom_area}>Ups! Paso algo  </p>
+			<section className={styles.error_container}>
+				<span><span>4</span></span>
+				<span>0</span>
+				<span><span>4</span></span>
+			</section>
+		</div>
+	)
 }
